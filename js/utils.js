1f63979b8536b0b7302f7e8dd238e8d798753ac3
bilirisaggelos-sketch <biliris.aggelos@gmail.com>
2026-06-25 19:52:46 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,29 @@
+function formatDate(dateStr) {
 
+    if (!dateStr) return "";
+
+    if (dateStr.includes("/")) {
+
+        const parts = dateStr.split("/");
+
+        const d = new Date(
+            parts[2],
+            parts[1] - 1,
+            parts[0]
+        );
+
+        return d.toLocaleDateString("en-GB", {
+            day: "2-digit",
+            month: "short",
+            year: "numeric"
+        });
+    }
+
+    const d = new Date(dateStr);
+
+    return d.toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric"
+    });
+}
